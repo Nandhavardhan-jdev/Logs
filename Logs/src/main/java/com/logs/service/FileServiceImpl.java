58ff2590de339f20b2932820c68ae8ffb640b8c6
Nandha <nandhavardhan.g@dikshatech.com>
2024-02-15 17:19:37 +0530

    committing mail part

--- Logs/src/main/java/com/logs/service/FileServiceImpl.java
+++ Logs/src/main/java/com/logs/service/FileServiceImpl.java
@@ -8,12 +8,22 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
 @Service
 public class FileServiceImpl implements FileService {
+	
+	@Autowired
+	JavaMailSender javaMailSender;
 
 	public InputStreamResource errorLevel(MultipartFile file) {
 		try {
@@ -26,13 +36,14 @@ public class FileServiceImpl implements FileService {
 				if (line.contains("ERROR")) {
 					data = true;
 					String[] words = line.split(" ");
-					writer.write("Time	: "+words[0]+"\n"+"UserId	: "+words[3]+"\n"+"URL	: "+words[4]+"\n"+"Error	: "+reader.readLine()+"\n"+reader.readLine()+"\n"+reader.readLine()+"\n"+reader.readLine()+"\n"+"\n");
+					writer.write("Time	: "+words[0]+"\n"+"Pwd	: "+words[3]+"\n"+"URL	: "+words[4]+"\n"+"Error	: "+words[8]+" "+words[9]+" "+words[10]+" "+words[11]+"\n"+"	"+reader.readLine()+"\n"+reader.readLine()+"\n"+reader.readLine()+"\n"+reader.readLine()+"\n"+"\n");
 				}
 			}
 			reader.close();
 			writer.close();
 			if (data) {
 				InputStreamResource isr = new InputStreamResource(new FileInputStream(file2));
+				this.sendMailWithAttachment(file2);
 				return isr;
 			}
 			else throw new RuntimeException("not found");
@@ -42,5 +53,28 @@ public class FileServiceImpl implements FileService {
 		}
 	}
 
+	public void sendMailWithAttachment(File file) {
+		
+		try {
+			MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+			MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+			mimeMessageHelper.setTo("mail@gmail.com");
+			String[] cc = {"mail1@gmail.com","mail2@gmail.com"};
+			mimeMessageHelper.setCc(cc);
+			mimeMessageHelper.setSubject("Email Generation");
+			mimeMessageHelper.setText("Hi All,\n\n"
+					+ "This mail is generated through smtp with attachment."
+					+ "\n\nThanks & Regards\nG Nandhavardhan Reddy");
+			FileSystemResource fileSystemResource = new FileSystemResource(file);
+			mimeMessageHelper.addAttachment(fileSystemResource.getFilename(), fileSystemResource);
+			javaMailSender.send(mimeMessage);
+			System.out.println("mail sended");
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new RuntimeException("not sended");
+		}
+		
+	}
 	
 }
